538b4e92f42e9a51359f1b96b3aca62b921c0687
AnthonyHMR <61439995+AnthonyHMR@users.noreply.github.com>
2023-11-27 06:35:49 -0800

    Cambios a Cuadratura Gaussiana

--- Tarea #3 - Grupo 5/Pregunta 1/gaussiana_compuesta.m
+++ Tarea #3 - Grupo 5/Pregunta 1/gaussiana_compuesta.m
@@ -1,30 +1,41 @@
 function I = gaussiana_compuesta(f, a, b, m)
-    [xv, p] = ceros_pol_legendre(m);
-    w = pesos(xv, p);
+  % La función gaussiana_compuesta aproxima la integral definida de una función mediante cuadratura gaussiana compuesta.
+  % La sintaxis de la función es: I = gaussiana_compuesta(f, a, b, m)
+  % Parámetros de entrada:
+  %       f: Función de entrada a integrar.
+  %       a: Límite de integración inferior.
+  %       b: Límite de integración superior.
+  %       m: Número de nodos de integración para la cuadratura gaussiana compuesta.
+  %
+  % Parámetros de salida:
+  %     I: Aproximación del valor de la integral definida.
 
-    h = (b - a) / m;
-    I = 0;
+  [xv, p] = ceros_pol_legendre(m);
+  w = pesos(xv, p);
 
-    for i = 1:m
-        xi = a + (i - 1) * h;
-        xi1 = a + i * h;
+  h = (b - a) / m;
+  I = 0;
 
-        I = I + h/2 * sum(w .* arrayfun(@(x) feval(f, (xi1 - xi) * x/2 + (xi + xi1)/2), xv));
-    end
+  for i = 1:m
+      xi = a + (i - 1) * h;
+      xi1 = a + i * h;
+
+      I = I + h/2 * sum(w .* arrayfun(@(x) feval(f, (xi1 - xi) * x/2 + (xi + xi1)/2), xv));
+  end
 end
 
 function [xv, p] = ceros_pol_legendre(n)
-    syms x
-    q = diff((x^2 - 1)^n, n);
-    p = (1 / (factorial(n) * 2^n) * q);
-    xv = solve(p == 0);
-    xv = double(xv);
+  syms x
+  q = diff((x^2 - 1)^n, n);
+  p = (1 / (factorial(n) * 2^n) * q);
+  xv = solve(p == 0);
+  xv = double(xv);
 end
 
 function w = pesos(xv, p)
-    syms x
-    n = length(xv);
-    pd = diff(p);
-    pd_n = matlabFunction(pd);
-    w = 2 ./ ((1 - xv.^2) .* (pd_n(xv)).^2);
+  syms x
+  n = length(xv);
+  pd = diff(p);
+  pd_n = matlabFunction(pd);
+  w = 2 ./ ((1 - xv.^2) .* (pd_n(xv)).^2);
 end
